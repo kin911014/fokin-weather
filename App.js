2b941934b8afc5cd9ccd9d952f76922d24b93466
김동은 <kimdong-eun@gimdong-eun-ui-MacBookPro.local>
2021-02-25 22:47:42 +0900

    make weather app

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
+import { Alert } from 'react-native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
+import * as Location from 'expo-location';
+import axios from 'axios';
+import Weather from './Weather';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//coordinates(좌표) 가져오는 api
+const API_KEY = 'b3d62d6e69630f1bc533fda8e1c59316';
+
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+      },
+      weather,
+    } = await axios.get(
+      //units=metric은 섭씨 설정표시이다...
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp,
+    });
+
+    console.log(data);
+  };
+  getLocation = async () => {
+    try {
+      // 사용자 허가
+      await Location.requestPermissionsAsync();
+
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      //Send To API and get Weather
+      this.getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't find your location", 'So sad');
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
